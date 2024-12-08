4593b7d42efe8b896ea2d7d0fca435737cb6b082
Ryan Broadbent <122940072+broadbr@users.noreply.github.com>
2024-12-07 22:06:44 -0500

    Update basics.dart add set operations

--- basics.dart
+++ basics.dart
@@ -16,4 +16,44 @@ void main() {
     }
 
 
+    // SET DATA STRUCTURE OPERATIONS
+    Set<int> FirstSet = {1, 3, 4, 5};
+
+    //Adds one int(2) to the set (the duplicate is ignored)
+    FirstSet.add(2);
+    FirstSet.add(2);
+
+    //Removes the 2
+    FirstSet.remove(2);
+
+    //Check if set contains an element
+    bool containsTwo = FirstSet.contains(2);
+    print('Set contains 3:2 $containsTwo');
+
+    print('Elements in the set:');
+    for (int number in FirstSet) {
+      print(number);
+    }
+
+    // Set operations
+
+    Set<int> SecondSet = {4, 5, 6, 2};
+
+    //Union
+    Set<int> BothSet = FirstSet.union(SecondSet);
+    print('Union: $BothSet');
+
+    //Intersection
+    Set<int> IntersectionSet = FirstSet.intersection(SecondSet);
+    print('Intersection of sets: $IntersectionSet');
+
+    //Difference
+    Set<int> DifferenceSet = FirstSet.difference(SecondSet);
+    print('Difference of sets: $DifferenceSet');
+
+    //Clear
+    FirstSet.clear();
+    print('Set after clear: $FirstSet');
+    ////////////////////
+
 }
